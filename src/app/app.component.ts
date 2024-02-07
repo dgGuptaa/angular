b8338a87f58a391cfec60212a6603ddfe2df2b59
dgGuptaa <159254084+dgGuptaa@users.noreply.github.com>
2024-02-07 19:27:18 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [],
+  template: `<h1>good evening</h1>`,
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'defaul   t';
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
